2d48769bf3cd30d671fbb87cfb68e78a94bf014e
Prince-jod <baghelprince164@gmail.com>
2026-07-16 01:18:40 +0530

    Auto commit

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/',(req,res)=>{
   console.log('Received a GET request at /');
     res.send('Hello World');
 });
+app.post('/post',(req,res)=>{
+  console.log('Received a POST request at /post');
+    res.send('POST request received');
+});
 
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`);
